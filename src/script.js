daf10a218654991fd24ff590b825c2b662a8f5d2
AliyaBiggsNavos <aliya.biggs@navos.co.uk>
2024-03-24 18:32:57 +0000

    refactor: :recycle: added buttons and improved comments

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import GUI from "lil-gui";
+//import GUI from "lil-gui";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
 import gsap from "gsap";
@@ -8,8 +8,6 @@ import gsap from "gsap";
 /**
  * Base
  */
-// Debug
-const gui = new GUI();
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -18,14 +16,29 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 //Group
+//Create group so all N's can be moved simultaneously
 const group = new THREE.Group();
 scene.add(group);
 
 /**
  * Textures
  */
+
+//Instantiate a TextureLoader instance
 const textureLoader = new THREE.TextureLoader();
-const matcapTexture = textureLoader.load("textures/matcaps/navosMatcap.png");
+const matcapTexture = textureLoader.load(
+  "textures/matcaps/navosMatcap.png",
+  () => {
+    console.log("loading finished");
+  },
+  () => {
+    console.log("loading progressing");
+  },
+  () => {
+    console.log("loading error");
+  }
+);
+//Matcap properties of a material need to be encoded in sRGB colorSpace
 matcapTexture.colorSpace = THREE.SRGBColorSpace;
 
 /**
@@ -39,20 +52,21 @@ const fontLoader = new FontLoader();
 
 fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
   // Material
-  const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+  const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture }); //Using MeshMatcapMaterial for it's great performance
 
   // Text
   const textGeometry = new TextGeometry("N", {
     font: font,
     size: 0.5,
     height: 0.05,
-    curveSegments: 12,
+    curveSegments: 12, //reduce as low poly as possible
     bevelEnabled: true,
     bevelThickness: 0.03,
     bevelSize: 0.02,
     bevelOffset: 0,
-    bevelSegments: 5,
+    bevelSegments: 5, //reduce as low poly as possible
   });
+  //Centre the text
   textGeometry.center();
 
   text1 = new THREE.Mesh(textGeometry, material);
@@ -60,22 +74,25 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
   text3 = new THREE.Mesh(textGeometry, material);
   text4 = new THREE.Mesh(textGeometry, material);
 
+  //Position N's so they begin as a square
   text2.position.x = 0.17;
   text2.position.z = -0.17;
-  text2.rotation.y = Math.PI * 0.5;
+  text2.rotation.y = Math.PI * 0.5; //90 degrees to the left
 
   text3.position.x = -0.17;
   text3.position.z = -0.17;
-  text3.rotation.y = Math.PI * -0.5;
+  text3.rotation.y = Math.PI * -0.5; //90 degrees to the right
 
   text4.position.z = -0.34;
-  text4.rotation.y = Math.PI * 1;
+  text4.rotation.y = Math.PI * 1; //180 degrees so reversed 
 
   group.add(text1, text2, text3, text4);
 
+  //Create GSAP timeline to act as a container
   const tl = gsap.timeline({ repeat: -1, repeatDelay: 0.5 });
   const delayInterval = 0.25;
 
+  //Setting up tweens for the N's
   tl.to(text1.position, { duration: 1, delay: delayInterval, z: 0.5 }, 0);
   tl.to(text2.position, { duration: 1, delay: delayInterval, x: 0.5 }, 0);
   tl.to(text3.position, { duration: 1, delay: delayInterval, x: -0.5 }, 0);
@@ -95,6 +112,13 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
     2
   );
   tl.to(text4.position, { duration: 1, delay: delayInterval, z: -0.34 }, 2);
+
+  // click handlers for controlling the tween instance
+  document.querySelector("#play").onclick = () => tl.play();
+  document.querySelector("#pause").onclick = () => tl.pause();
+  document.querySelector("#resume").onclick = () => tl.resume();
+  document.querySelector("#reverse").onclick = () => tl.reverse();
+  document.querySelector("#restart").onclick = () => tl.restart();
 });
 
 /**
@@ -105,6 +129,7 @@ const sizes = {
   height: window.innerHeight,
 };
 
+//Handles window resizing
 window.addEventListener("resize", () => {
   // Update sizes
   sizes.width = window.innerWidth;
@@ -150,20 +175,15 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 /**
  * Animate
  */
-const clock = new THREE.Clock();
-
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-
-
   // Update controls
   controls.update();
 
   // Render
   renderer.render(scene, camera);
 
-  // Call tick again on the next frame
+  // GSAP function to update animation automatically
   window.requestAnimationFrame(tick);
 };
 
